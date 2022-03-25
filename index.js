33d2d9ce176bee29d14f12a9947aa78c97720371
Dayana Roslan <dayanaiffah@gmail.com>
2022-03-25 15:39:35 +0800

    render new question

--- index.js
+++ index.js
@@ -15,16 +15,23 @@ $(document).ready(function(){
     return question;
   }
   
-  currentQuestion = questionGenerator();
-  $('#equation').text(currentQuestion.equation);
+  var renderNewQuestion = function () {
+    currentQuestion = questionGenerator();
+    $('#equation').text(currentQuestion.equation);  
+  }
 
 
   var checkAnswer = function (userInput, answer) {
-    console.log(userInput === answer);
+    if(userInput === answer) {
+      renderNewQuestion();
+      $('#user-input').val('');
+    }
   }
   
   $('#user-input').on('keyup', function () {
     checkAnswer(Number($(this).val()), currentQuestion.answer);
   });
+
+  renderNewQuestion();
 
 });
